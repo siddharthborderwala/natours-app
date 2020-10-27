0972bfebed4a5118ab61d9ba45200c5a2e899827
Siddharth Borderwala <siddharthborderwala@gmail.com>
2020-10-27 16:29:18 +0530

    create review model

---
+++ models/reviewModel.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+
+const reviewSchema = new mongoose.Schema(
+  {
+    review: {
+      type: String,
+      required: [true, 'Review cannot be empty'],
+    },
+    rating: {
+      type: Number,
+      min: [0, 'Ratings cannot be lower than 0'],
+      max: [5, 'Ratings cannot be above 5'],
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+    },
+    tour: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'Tour',
+      required: [true, 'Review must belong to a tour'],
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User',
+      required: [true, 'Review must belong to a user'],
+    },
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    toObject: {
+      virtuals: true,
+    },
+  }
+);
+
+reviewSchema.pre(/^find/, function (next) {
+  this.populate({
+    path: 'user',
+    select: '-email -role -_id -__v',
+  });
+  next();
+});
+
+const Review = mongoose.model('Review', reviewSchema);
+
+module.exports = Review;
