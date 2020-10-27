f33c84d45a31d0f25ddcd52ef3ea3ca2ffd9a82f
Siddharth Borderwala <siddharthborderwala@gmail.com>
2020-10-27 16:37:06 +0530

    rename and more modular code

---
+++ dev-data/data/script.js
@@ -0,0 +1,78 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const Tour = require('../../models/tourModel');
+const User = require('../../models/userModel');
+
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('DB Connection Successful'))
+  .catch(err => console.log(err));
+
+//READ TOUR DATA FILE
+
+const toursData = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours.json`, 'utf-8')
+);
+
+const usersData = JSON.parse(
+  fs.readFileSync(`${__dirname}/users.json`, 'utf-8')
+);
+
+//IMPORT DATA INTO DB
+
+const importData = async resource => {
+  try {
+    if (resource === 'tours') await Tour.create(toursData);
+    else if (resource === 'users') await User.create(usersData);
+    else if (resource === 'all') {
+      await Tour.create(toursData);
+      await User.create(usersData);
+    } else throw new Error('Invalid resource');
+    return 'Success importing data into DB';
+  } catch (error) {
+    return `ERROR: ${error.message}`;
+  }
+};
+
+//DELETE ALL DATA FROM TOUR COLLECTION
+
+const deleteData = async resource => {
+  try {
+    if (resource === 'tours') await Tour.deleteMany();
+    else if (resource === 'users') await User.deleteMany();
+    else if (resource === 'all') {
+      await Tour.deleteMany();
+      await User.deleteMany();
+    } else throw new Error('Invalid resource');
+    return 'Successfully deleted data from DB';
+  } catch (error) {
+    return `ERROR: ${error.message}`;
+  }
+};
+
+const main = async () => {
+  let result;
+
+  if (process.argv[2] === '--import')
+    result = await importData(process.argv[3]);
+  else if (process.argv[2] === '--delete')
+    result = await deleteData(process.argv[3]);
+
+  console.log(result);
+  process.exit(0);
+};
+
+main();
