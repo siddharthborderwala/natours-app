52eb3653ddec689d501106fe3a1c4b94e31718ae
Siddharth Borderwala <siddharthborderwala@gmail.com>
2020-11-03 21:24:05 +0530

    call next in setTourAndUserIds function

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -4,6 +4,7 @@ const factory = require('./handlerFactory');
 exports.setTourAndUserIds = (req, res, next) => {
   if (!req.body.tour) req.body.tour = req.params.tourId;
   if (!req.body.user) req.body.user = req.user.id;
+  next();
 };
 
 exports.getAllReviews = factory.getAll(Review);
