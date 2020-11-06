e3b57f62de9c3bece299c62234e3f36acdf2b1d8
Siddharth Borderwala <siddharthborderwala@gmail.com>
2020-11-06 09:24:24 +0530

    script for mapbox on tour page

---
+++ public/js/mapbox.js
@@ -0,0 +1,48 @@
+/* eslint-disable */
+const locations = JSON.parse(
+  document.getElementById('map').dataset['locations']
+);
+
+mapboxgl.accessToken =
+  'pk.eyJ1Ijoic2lkZGhhcnRoYm9yZGVyd2FsYSIsImEiOiJja2g0ZnFsNDQwMTVrMnluMW9tMDlvdDF2In0.IJ-netZroHXa82baJL9AqA';
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/siddharthborderwala/ckh4kv0ac0pah19o717g45mhb',
+  scrollZoom: false,
+});
+
+const bounds = new mapboxgl.LngLatBounds();
+
+locations.forEach(loc => {
+  // create marker
+  const el = document.createElement('div');
+  el.className = 'marker';
+
+  // add marker
+  new mapboxgl.Marker({
+    element: el,
+    anchor: 'bottom',
+  })
+    .setLngLat(loc.coordinates)
+    .addTo(map);
+
+  // add popup
+  new mapboxgl.Popup({
+    offset: 30,
+  })
+    .setLngLat(loc.coordinates)
+    .setHTML(`<p>Day ${loc.day}: ${loc.description}</p>`)
+    .addTo(map);
+
+  // extend the map bounds to include the current location
+  bounds.extend(loc.coordinates);
+});
+
+map.fitBounds(bounds, {
+  padding: {
+    top: 200,
+    bottom: 150,
+    left: 200,
+    right: 200,
+  },
+});
