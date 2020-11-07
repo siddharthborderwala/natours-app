c8ea3b43a79bda29c2f916cc109c2366a6c1755b
Siddharth Borderwala <siddharthborderwala@gmail.com>
2020-11-07 22:30:39 +0530

    create custom alert element

---
+++ public/js/alert.js
@@ -0,0 +1,25 @@
+/* eslint-disable */
+
+export const showAlert = (type, msg, timeout = 4000) => {
+  // remove a previously created alert
+  const currentAlert = document.querySelector('.alert');
+  if (currentAlert) currentAlert.remove();
+
+  // create a new alert element
+  const element = document.createElement('div');
+  element.classList.add('alert', `alert--${type}`);
+  element.textContent = msg;
+
+  // insert it into the body
+  document.querySelector('body').insertAdjacentElement('afterbegin', element);
+
+  // shrink it before removing
+  setTimeout(() => {
+    element.classList.add('collapse');
+  }, timeout - 100);
+
+  // //remove it after some time
+  // setTimeout(() => {
+  //   element.remove();
+  // }, timeout);
+};
