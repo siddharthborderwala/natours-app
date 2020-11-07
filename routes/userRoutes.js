b1416ec2943a8435a90a872212896f6386fd6a1e
Siddharth Borderwala <siddharthborderwala@gmail.com>
2020-11-07 22:32:26 +0530

    create logout route on api

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -9,6 +9,7 @@ const router = require('express').Router();
 const userController = require('../controllers/userController');
 const authController = require('../controllers/authController');
 
+router.get('/logout', authController.logout);
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
 router.post('/forgot-password', authController.forgotPassword);
