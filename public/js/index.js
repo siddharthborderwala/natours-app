a2ef71ff488003ad818746bc0e76fca4d296aaa7
Siddharth Borderwala <siddharthborderwala@gmail.com>
2020-11-09 18:12:45 +0530

    handle account data and password form submissions

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,14 @@
 /* eslint-disable */
 import '@babel/polyfill';
 import { login, logout } from './auth';
+import { updateMyData, updateMyPassword } from './updateSettings';
 import { displayMap } from './mapbox';
 
 const map = document.getElementById('map');
-const form = document.querySelector('form.form');
+const form = document.querySelector('form.form__login');
 const logoutButton = document.querySelector('.nav__el--logout');
+const updateMyDataForm = document.querySelector('form.form-user-data');
+const updateMyPasswordForm = document.querySelector('form.form-user-settings');
 
 if (map) {
   const locations = JSON.parse(map.dataset['locations']);
@@ -21,3 +24,22 @@ if (form)
   });
 
 if (logoutButton) logoutButton.addEventListener('click', logout);
+
+if (updateMyDataForm) {
+  updateMyDataForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    updateMyData(name, email);
+  });
+}
+
+if (updateMyPasswordForm) {
+  updateMyPasswordForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const passwordCurrent = document.getElementById('password-current').value;
+    const password = document.getElementById('password').value;
+    const passwordConfirm = document.getElementById('password-confirm').value;
+    updateMyPassword(passwordCurrent, password, passwordConfirm);
+  });
+}
