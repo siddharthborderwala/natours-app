aeeb1f51786b26ad992decfbbc2b06d02d0a7729
Siddharth Borderwala <siddharthborderwala@gmail.com>
2020-11-09 18:09:00 +0530

    create me route and handle it

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -4,10 +4,10 @@ const authController = require('../controllers/authController');
 
 const router = express.Router();
 
-router.use(authController.isLoggedIn);
+router.get('/', authController.isLoggedIn, viewsController.getOverview);
+router.get('/tour/:slug', authController.isLoggedIn, viewsController.getTour);
+router.get('/login', authController.isLoggedIn, viewsController.getLoginForm);
 
-router.get('/', viewsController.getOverview);
-router.get('/tour/:slug', viewsController.getTour);
-router.get('/login', viewsController.getLoginForm);
+router.route('/me').all(authController.protect).get(viewsController.getAccount);
 
 module.exports = router;
