9266881a6ba53e5cfb7e7393694189434b14e252
Siddharth Borderwala <siddharthborderwala@gmail.com>
2020-11-13 17:43:08 +0530

    add and handle new routes

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -2,11 +2,17 @@ const router = require('express').Router();
 
 const viewsController = require('../controllers/viewsController');
 const authController = require('../controllers/authController');
+const { notImplementedRoute } = require('../utils/notImplementedRoute');
 
 router.get('/', authController.isLoggedIn, viewsController.getOverview);
 router.get('/tour/:slug', authController.isLoggedIn, viewsController.getTour);
 router.get('/login', authController.isLoggedIn, viewsController.getLoginForm);
+// TODO: implement signup
+router.get('/signup', notImplementedRoute);
 
 router.route('/me').all(authController.protect).get(viewsController.getAccount);
+// TODO: implement reset password
+// access resetToken from url params on frontEnd
+router.get('/forgot-password', notImplementedRoute);
 
 module.exports = router;
