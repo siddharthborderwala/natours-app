0df56412ccc3b50fa943c00f41f5007f1965c4cd
Siddharth Borderwala <siddharthborderwala@gmail.com>
2020-11-26 12:06:47 +0530

    create signup function and refactor

--- public/js/auth.js
+++ public/js/auth.js
@@ -2,15 +2,12 @@
 import axios from 'axios';
 import { showAlert } from './alert';
 
-export const login = async (email, password) => {
+export const login = async data => {
   try {
     const res = await axios({
-      url: `/api/v1/users/login`,
+      url: '/api/v1/users/login',
       method: 'POST',
-      data: {
-        email,
-        password,
-      },
+      data,
     });
 
     if (res.data.status === 'success') {
@@ -26,11 +23,31 @@ export const logout = async () => {
   try {
     const res = await axios({
       method: 'GET',
-      url: `/api/v1/users/logout`,
+      url: '/api/v1/users/logout',
     });
 
-    if (res.data.status === 'success') location.reload(true);
+    if (res.data.status === 'success') {
+      showAlert('success', 'Logged out successfully');
+      window.setTimeout(location.assign.bind(location, '/'), 500);
+    }
   } catch (err) {
     showAlert('error', 'Error logging out, try again.');
+  }
+};
+
+export const signup = async data => {
+  try {
+    const res = await axios({
+      method: 'POST',
+      url: '/api/v1/users/signup',
+      data,
+    });
+
+    if (res.data.status === 'success') {
+      showAlert('success', 'Account created successfully');
+      window.setTimeout(location.assign.bind(location, '/'), 1000);
+    }
+  } catch (err) {
+    showAlert('error', err.response.data.message);
   }
 };
