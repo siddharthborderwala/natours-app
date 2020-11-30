93f2cb61652295d23ddf50b7166929873f44c9a4
Siddharth Borderwala <siddharthborderwala@gmail.com>
2020-11-30 17:57:41 +0530

    add route handlers

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -2,23 +2,17 @@ const router = require('express').Router();
 
 const viewsController = require('../controllers/viewsController');
 const authController = require('../controllers/authController');
-const { notImplementedRoute } = require('../utils/notImplementedRoute');
 
 router.get('/', authController.isLoggedIn, viewsController.getOverview);
 router.get('/tour/:slug', authController.isLoggedIn, viewsController.getTour);
 router.get('/login', authController.isLoggedIn, viewsController.getLoginForm);
-// TODO: implement signup
 router.get('/signup', viewsController.getSignupForm);
 
-router.route('/me').all(authController.protect).get(viewsController.getAccount);
+router.get('/me', authController.protect, viewsController.getAccount);
 router.get('/my-tours', authController.protect, viewsController.getMyTours);
-// TODO: implement reset password
-// send req from login form, receive email, open this rote
-// access resetToken from url params on frontEnd
-router.get('/forgot-password', notImplementedRoute);
 
-// TODO: implement checkout success page
-// access resetToken from url params on frontEnd
-router.get('/checkout/success', notImplementedRoute);
+router.get('/forgot-password', viewsController.getForgotPassword);
+router.get('/reset-password', viewsController.getResetPassword);
+router.get('/checkout', viewsController.getCheckout);
 
 module.exports = router;
